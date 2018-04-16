dacac10bffa7a83f1b8c5cfdd9964fcfe181abf6
Kobey Myhre <s179139@students.aie.edu.au>
2018-04-16 14:38:03 -0700

    Init commit

---
+++ Assets/Seeker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeker : MonoBehaviour {
+
+
+    GameObject player;
+    Rigidbody rb;
+    public float speed;
+    public float lifeTime;
+    float lifeTimer;
+    PooledObject obj;
+    // Use this for initialization
+    void Start ()
+    {
+        obj = GetComponent<PooledObject>();
+        rb = GetComponent<Rigidbody>();
+        float rando = Random.Range(1, 7);
+        transform.localScale = Vector3.one * rando;
+        float r = Random.Range(50, 206);
+        float g = Random.Range(50, 206);
+        float b = Random.Range(50, 206);
+        GetComponent<Renderer>().material.color = new Color(r,g,b,1);
+        player = GameObject.Find("Player");
+        lifeTimer = lifeTime;
+	}
+	
+
+    public void resetSeeker()
+    {
+        lifeTimer = lifeTime;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+        rb.AddForce(dir.normalized * speed);
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+            //if (obj != null)
+            //{
+            //    resetSeeker();
+            //    obj.returnToPool();
+            //}
+            //else
+            //{
+            //    Destroy(gameObject);
+            //}
+
+        }
+    }
+
+    
+}
---
+++ Assets/SeekerSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeekerSpawner : MonoBehaviour {
+
+
+    public GameObject seeker;
+    public float spawnInterval;
+    public int spawnCount;
+   
+    float spawnTime;
+    ObjectPool pool;
+
+    private void Start()
+    {
+        pool = GetComponent<ObjectPool>();
+    }
+
+    void poolSpawn()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            GameObject spawnedSeeker = pool.getObject();
+            spawnedSeeker.SetActive(true);
+            spawnedSeeker.transform.position = transform.position + (Random.insideUnitSphere * spawnCount);
+        }
+    }
+    void spawn()
+    {
+        for(int i =0; i < spawnCount; i++)
+        {
+            GameObject spawnedSeeker = Instantiate(seeker);
+            spawnedSeeker.transform.position = transform.position + (Random.insideUnitSphere * spawnCount);
+        }
+       
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        spawnTime -= Time.deltaTime;
+       
+        if(spawnTime <= 0)
+        {
+            spawn();
+            spawnTime = spawnInterval;
+        }
+       
+	}
+}
